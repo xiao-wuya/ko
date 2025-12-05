28316e5596be57eecd1319347bd5be6838b2512d
xiao-wuya <xiaowuya443@gmail.com>
2025-12-06 06:57:44 +0800

    Fix syntax error in headers object

--- worker.js
+++ worker.js
@@ -627,7 +627,7 @@ clash-meta
 ################################################################
 `;
 }							headers: {
-								"Content-Type": "text/plain;charset=utf-8";
+								"Content-Type":"text/plain;charset=utf-8",
 							}
 						});
 					}
